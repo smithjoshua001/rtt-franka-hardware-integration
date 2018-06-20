0135be847105c1580dd175f34643a7e85a631969
Julian Theis <jlntheis@gmail.com>
2018-06-20 14:31:05 +0000

    Document setup script

--- test/setup-franka-env.sh
+++ test/setup-franka-env.sh
@@ -1,2 +1,5 @@
-export RTT_COMPONENT_PATH=$RTT_COMPONENT_PATH:~/rtt-franka-hardware-integration/build/orocos:~/rtt-franka-hardware-integration/test/robot_data_test/build/src/orocos
-export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:~/cogimon-gazebo-models
+# Lazy script to set up environment for testing.
+# You probably have to adjust the values for use on your own machine.
+
+export RTT_COMPONENT_PATH=$RTT_COMPONENT_PATH:/code/rtt-franka-hardware-integration/build/orocos:/code/rtt-franka-hardware-integration/test/robot_data_test/build/src/orocos
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/code/cogimon-gazebo-models
